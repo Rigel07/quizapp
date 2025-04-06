6c13455f9bdb551494eefd6c3a30f9a4178bf281
Pranav Chandak <pranavchandak45@gmail.com>
2025-04-06 22:54:21 +0530

    api key exposure fixed

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAporv4SwXz2nPEZfbVUOQUvZATegC59gc",
-  authDomain: "quizapp-747d4.firebaseapp.com",
-  projectId: "quizapp-747d4",
-  storageBucket: "quizapp-747d4.firebasestorage.app",
-  messagingSenderId: "618087630067",
-  appId: "1:618087630067:web:126c592e27ae6ae112a386",
-  measurementId: "G-PPZXB6XXDZ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
